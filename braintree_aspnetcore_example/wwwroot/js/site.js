9c2c4493308ec1c27ae7666074458b49aad9971a
hollabaq86 <hollis65@me.com>
2020-09-25 14:55:09 -0500

    Add checkouts controller, view, and styling

--- braintree_aspnetcore_example/wwwroot/js/site.js
+++ braintree_aspnetcore_example/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+'use strict';
+
+(function () {
+    const amount = document.querySelector('#amount');
+    const amountLabel = document.querySelector('label[for="amount"]');
+
+    amount.addEventListener('focus', function () {
+        amountLabel.className = 'has-focus';
+    }, false);
+    amount.addEventListener('blur', function () {
+        amountLabel.className = '';
+    }, false);
+})();
